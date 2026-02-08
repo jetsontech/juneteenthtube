47c3fdf75fda8e9a4efc1fe4d997d83553997a5d
Jetson <jetsontech@gmail.com>
2026-02-08 14:46:44 -0500

    Polish: Finalize walkthrough documentation and resolve remaining CSS compatibility warnings in globals.css.

--- src/components/interactive/FreedomMap.tsx
+++ src/components/interactive/FreedomMap.tsx
@@ -123,7 +123,7 @@ export function FreedomMap() {
                     <div
                         key={pin.id}
                         className="absolute group cursor-pointer z-10"
-                        /* eslint-disable-next-line react/no-unknown-property */
+                        /* eslint-disable-next-line react/no-unknown-property, react/no-inline-styles */
                         style={{ "--pin-top": pin.top, "--pin-left": pin.left } as React.CSSProperties}
                         onClick={() => setActivePin(pin)}
                         role="button"
--- src/components/layout/GlossOverlay.tsx
+++ src/components/layout/GlossOverlay.tsx
@@ -10,7 +10,7 @@ export const GlossOverlay = ({ className, style }: GlossOverlayProps) => {
     return (
         <div
             className={cn("fixed inset-0 z-[-2] pointer-events-none gloss-overlay", className)}
-            /* eslint-disable-next-line react/no-unknown-property */
+            /* eslint-disable-next-line react/no-unknown-property, react/no-inline-styles */
             style={style}
         />
     );
